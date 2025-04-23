357a28fc4153d1fe21108c4f3df1156661ec112b
SanskrutiMore07 <sanskrutim2004@gmail.com>
2025-04-23 15:39:33 +0530

    program

---
+++ String/String_Program1.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+#include<string.h>
+
+int main()
+{
+    char demo[15]={'K','A','R','A','D'};
+
+    puts(demo);              ///KARAD
+
+    printf("\n Enter a String :");      ///Hello
+    gets(demo);
+    printf("\n Given String is: ");
+    puts(demo);                          ///Hello
+
+    getch();
+    return 0;
+}
