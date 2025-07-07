231bf72a928ded07e3b6d2818175cd3ba495ea31
SanskrutiMore07 <sanskrutim2004@gmail.com>
2025-07-07 21:44:36 +0530

    program

---
+++ Assignment 2/Program 07.c
@@ -0,0 +1,16 @@
+///7.Write a program that accepts a number from the user and checks if it is even or odd using the ternary operator.
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    int No =0;
+
+    printf("\n Enter Any Number :");
+    scanf("%d",&No);
+
+    (No % 2 == 0)?(printf("%d is Even Number!!!!",No)):(printf("% is Odd Number!!!!",No));
+
+    getch();
+    return 0;
+}
