e834715404cdb4750346973ad1286721c3ccc5dd
SanskrutiMore07 <sanskrutim2004@gmail.com>
2025-08-11 23:21:30 +0530

    Array Program

---
+++ Assignment_Array/02 Create Array with Initialization & Display All Elements.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    int i,arr[6] ={12,15,42,56,58,99};
+
+    printf("\n Given Array Elements Are :");
+
+    for(i=0;i<arr[i];i++)
+    {
+        printf("\n Element %d Is %d",i+0,arr[i]);
+    }
+
+    getch();
+    return 0;
+}
