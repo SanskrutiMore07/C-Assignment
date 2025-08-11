b2e298e5fba1b21ccd3646d9a8c20adf4f97b224
SanskrutiMore07 <sanskrutim2004@gmail.com>
2025-08-11 23:33:54 +0530

    Array Program

---
+++ Assignment_Array/03 Crate Array of 7 Elements with Null (0) Initialization & Display All Elements.c
@@ -0,0 +1,27 @@
+///03	Crate Array of 7 Elements with Null (0) Initialization & Display All Elements
+
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    int i , arr[5];
+
+    printf("\n Enter Numbers: ");
+
+    for(i = 0 ;i<arr[i];i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+
+    printf("\n Elements Of array => ");
+
+    for(i =0 ;i<arr[i];i++)
+    {
+        printf("\n Element %d  => %d ",i+0,arr[i]);
+    }
+
+    getch();
+    return 0;
+
+}
