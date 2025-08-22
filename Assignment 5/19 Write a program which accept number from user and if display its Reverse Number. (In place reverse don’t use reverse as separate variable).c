ff6fa8ecc3045f86659dbf6303030752d52c734f
SanskrutiMore07 <sanskrutim2004@gmail.com>
2025-08-22 19:49:07 +0530

    Program 19

---
+++ Assignment 5/19 Write a program which accept number from user and if display its Reverse Number. (In place reverse don’t use reverse as separate variable).c
@@ -0,0 +1,9 @@
+///19.	Write a program which accept number from user and if display its Reverse Number. (In place reverse don’t use reverse as separate variable)
+
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+
+}
