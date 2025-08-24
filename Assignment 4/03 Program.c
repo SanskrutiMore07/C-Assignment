0b045c660afd912a4719addf4772c3dbd43a2c7f
SanskrutiMore07 <sanskrutim2004@gmail.com>
2025-08-24 21:17:15 +0530

    Program 3

---
+++ Assignment 4/03 Program.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    int No1,No2,i,j=0;
+
+    printf("\n Enter 2 Numbers : ");
+    scanf("%d%d",&No1,&No2);
+
+    for(i=1;i<=No1;i++)
+    {
+        for(j=1;j<=No2;j++)
+        {
+            printf(" %d ",i);
+        }
+        printf("\n");
+    }
+    getch();
+    return 0;
+}
