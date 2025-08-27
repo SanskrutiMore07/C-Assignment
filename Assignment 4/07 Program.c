e8344290eb7d7a7092dd7457550c2cf530e3ed2c
SanskrutiMore07 <sanskrutim2004@gmail.com>
2025-08-27 22:15:18 +0530

    Program 7

---
+++ Assignment 4/07 Program.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    int i,j,No=0;
+
+    printf("\n Enter Any Number :");
+    scanf("%d",&No);
+
+    for(i=1;i<=No;i++)
+    {
+        for(j=5;j>=1;j--)
+        {
+            printf("%d",j);
+        }
+        printf("\n");
+    }
+    getch();
+    return 0;
+}
